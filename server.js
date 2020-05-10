7cc6671beea312739d2de09fe694e09f0f00f87c
wb371710 <wb-lal371710@alibaba-inc.com>
2020-05-10 19:29:01 +0800

    feat: add gitignore

---
+++ server.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+app.use(async ctx => {
+  ctx.body = 'hello world';
+});
+
+app.listen(3000);

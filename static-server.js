ccfee49e78f5308564d2725734650e78246c234d
wb371710 <wb-lal371710@alibaba-inc.com>
2020-05-12 22:19:27 +0800

    feat: add route

--- static-server.js
+++ static-server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 const config = require('./config/default.json');
 
 class StaticServer {
@@ -9,11 +10,35 @@ class StaticServer {
     this.indexPage = config.indexPage;
   }
 
+  respondNotFound(req, res) {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+    });
+    const html = `<h1>Not Found</h1><p>The requested URL ${req.url} was not found on this server.</p>`;
+    res.end(html);
+  }
+
+  respondFile(pathName, req, res) {
+    const readStream = fs.createReadStream(pathName);
+    readStream.pipe(res);
+  }
+
+  routeHandler(pathName, req, res) {
+    fs.access(pathName, fs.constants.F_OK, (err) => {
+      if (!err) {
+        this.respondFile(pathName, req, res);
+      } else {
+        this.respondNotFound(req, res);
+      }
+    })
+  }
+
   start() {
     http.createServer((req, res) => {
       const pathName = path.join(this.root, path.normalize(req.url));
       res.writeHead(200);
-      res.end(`Requeste path: ${pathName}`);
+      // res.end(`Requeste path: ${pathName}`);
+      this.routeHandler(pathName, req, res);
     }).listen(this.port, err => {
       if (err) {
         console.error(err);
